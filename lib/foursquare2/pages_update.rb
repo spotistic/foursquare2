7c4f3507477fe9fdc2ca337074a48ca9753675a2
Baptiste <baptiste@spotistic.com>
2014-06-03 09:27:36 +0200

    better doc

--- lib/foursquare2/pages_update.rb
+++ lib/foursquare2/pages_update.rb
@@ -1,11 +1,11 @@
 module Foursquare2
   module PagesUpdate
 
-    # Add a tip
+    # Add a tip see https://developer.foursquare.com/docs/pageupdates/add
     #
     # @param [Hash]  options
-    # @option options String :pageId - Required, which page to add the tip to.
-    # @option options String :venueId - Required, which venue to add the tip to.
+    # @option options String :pageId - which page to add the tip to.
+    # @option options String :venueId - A comma-separated list of venue IDs indicated which venues to broadcast from.
     # @option options String :shout - The text of the update.
     # @option options String :photoId -  An optional photo to attach to the broadcast.
 
